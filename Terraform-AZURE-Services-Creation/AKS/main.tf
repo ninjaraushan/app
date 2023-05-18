5942ca931e5d9570f4c533735d292dfb5b2aef63
unknown <raushan.raj0507@gmail.com>
2023-05-19 01:01:52 +0530

    formatting

--- Terraform-AZURE-Services-Creation/AKS/main.tf
+++ Terraform-AZURE-Services-Creation/AKS/main.tf
@@ -113,7 +113,7 @@ resource "azurerm_role_assignment" "node_infrastructure_update_scale_set" {
 }
 
 data "azurerm_container_registry" "example" {
-  name                = "${var.name}"
+  name                = var.name
   resource_group_name = data.azurerm_resource_group.resource_group.name
 }
 
